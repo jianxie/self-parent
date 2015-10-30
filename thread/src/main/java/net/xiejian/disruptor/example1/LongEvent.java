04f7f57b31ceca560687701faf84ebaecbc063c4
jian_xie <xj328@163.com>
2015-10-30 22:08:30 +0800

    disruptor

---
+++ thread/src/main/java/net/xiejian/disruptor/example1/LongEvent.java
@@ -0,0 +1,17 @@
+package net.xiejian.disruptor.example1;
+
+/**
+ * 定义事件
+ * @author jian_xie
+ * @date 2015年10月30日
+ */
+public class LongEvent {
+	
+	private long value;
+
+	public void setValue(long value) {
+		this.value = value;
+	}
+	
+	
+}
